ec94d8f1cbf93bcdce9ad07921336ca67081bf71
babykittenz <kidbymichaelb@gmail.com>
2025-03-13 13:57:11 -0500

    feat(test): improved test with test table

--- main_test.go
+++ main_test.go
@@ -3,24 +3,26 @@ package main
 import "testing"
 
 func Test_isPrime(t *testing.T) {
-	result, msg := isPrime(0)
-	if result {
-		t.Errorf("Expected false, got %t", result)
+	primeTests := []struct {
+		name     string
+		testNum  int
+		expected bool
+		msg      string
+	}{
+		{"prime", 7, true, "7 is a prime number"},
+		{"not prime", 8, false, "8 is not prime because it is not divisible by 2"},
+		{"negative", -1, false, "Negative numbers are not prime numbers"},
 	}
-	if msg != "0 is not a prime number" {
-	}
-
-	result, msg = isPrime(1)
-	if result {
-		t.Errorf("Expected false, got %t", result)
-	}
-	if msg != "1 is not a prime number" {
-	}
-
-	result, msg = isPrime(2)
-	if !result {
-		t.Errorf("Expected true, got %t", result)
-	}
-	if msg != "2 is a prime number" {
+	for _, e := range primeTests {
+		result, msg := isPrime(e.testNum)
+		if e.expected && !result {
+			t.Errorf("%s: expected true got false", e.name)
+		}
+		if !e.expected && result {
+			t.Errorf("%s: expected false got true", e.name)
+		}
+		if e.msg != msg {
+			t.Errorf("%s: expected %s got %s", e.name, e.msg, msg)
+		}
 	}
 }

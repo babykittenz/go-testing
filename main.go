7982c9480057878c7983fc94885357d01f2e46dd
babykittenz <kidbymichaelb@gmail.com>
2025-03-16 09:25:21 -0500

    feat: add more robust testing and 82% coverage

--- main.go
+++ main.go
@@ -1,12 +1,74 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	n := 2
-	_, msg := isPrime(n)
-	fmt.Println(msg)
+	// print a welcome message
+	intro()
+	// create a channel to indicate when the user wants to quit
+	doneChan := make(chan bool)
+	// start a go routine to read user input and run program
+	go readUserInput(os.Stdin, doneChan)
+	// block until the done chan gets a value
+	<-doneChan
+
+	// close the channel
+	close(doneChan)
+	// say goodbye
+	fmt.Println("Goodbye")
 }
+
+func readUserInput(in io.Reader, doneChan chan bool) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		res, done := checkNumbers(scanner)
+
+		if done {
+			doneChan <- true
+			return
+		}
+
+		fmt.Println(res)
+		prompt()
+	}
+}
+func checkNumbers(scanner *bufio.Scanner) (string, bool) {
+	// read scanner
+	scanner.Scan()
+
+	// check to see if the user wants to quit
+	if strings.EqualFold(scanner.Text(), "q") {
+		return "", true
+	}
+
+	// try to convert what the user typed into an int
+	numToCheck, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		return "Please enter a whole number", false
+	}
+
+	_, msg := isPrime(numToCheck)
+	return msg, false
+}
+func intro() {
+	fmt.Println("Is it Prime?")
+	fmt.Println("----------------")
+	fmt.Println("Enter a whole number, and we'll tell you if it is prime or not. Enter q to quit.")
+	prompt()
+}
+
+func prompt() {
+	fmt.Print("->")
+}
+
 func isPrime(n int) (bool, string) {
 
 	// prime numbers do not include 0 or 1
--- main_test.go
+++ main_test.go
@@ -1,6 +1,13 @@
 package main
 
-import "testing"
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"os"
+	"strings"
+	"testing"
+)
 
 func Test_isPrime(t *testing.T) {
 	primeTests := []struct {
@@ -12,6 +19,8 @@ func Test_isPrime(t *testing.T) {
 		{"prime", 7, true, "7 is a prime number"},
 		{"not prime", 8, false, "8 is not prime because it is not divisible by 2"},
 		{"negative", -1, false, "Negative numbers are not prime numbers"},
+		{"zero", 0, false, "0 is not a prime number"},
+		{"one", 1, false, "1 is not a prime number"},
 	}
 	for _, e := range primeTests {
 		result, msg := isPrime(e.testNum)
@@ -25,4 +34,98 @@ func Test_isPrime(t *testing.T) {
 			t.Errorf("%s: expected %s got %s", e.name, e.msg, msg)
 		}
 	}
+}
+
+func Test_prompt(t *testing.T) {
+	// save a copy of os.Stdout
+	oldOut := os.Stdout
+
+	// create a a read and write pipe
+	r, w, _ := os.Pipe()
+
+	// set os.Stdout to write pipe
+	os.Stdout = w
+
+	prompt()
+
+	// close our writer
+	_ = w.Close()
+
+	// reset os.Stdout to what it was before
+	os.Stdout = oldOut
+
+	// read the output of the prompt function from our read pipe
+	out, _ := io.ReadAll(r)
+
+	// perform out test
+	if string(out) != "->" {
+		t.Errorf("incorrect prompt expected -> got %s", string(out))
+	}
+}
+
+func Test_intro(t *testing.T) {
+	// save a copy of os.Stdout
+	oldOut := os.Stdout
+
+	// create a read and write pipe
+	r, w, _ := os.Pipe()
+
+	// set os.Stdout to write pipe
+	os.Stdout = w
+
+	intro()
+
+	// close our writer
+	_ = w.Close()
+
+	// reset os.Stdout to what it was before
+	os.Stdout = oldOut
+
+	// read the output of the prompt function from our read pipe
+	out, _ := io.ReadAll(r)
+
+	// perform out test
+	if !strings.Contains(string(out), "Enter a whole number, and we'll tell you if it is prime or not. Enter q to quit.") {
+		t.Errorf("intro test not correct got %s", string(out))
+	}
+}
+
+func Test_checkNumbers(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{name: "empty", input: "", expected: "Please enter a whole number"},
+		{name: "not a number", input: "not a number", expected: "Please enter a whole number"},
+		{name: "q", input: "q", expected: ""},
+		{name: "7", input: "7", expected: "7 is a prime number"},
+		{name: "8", input: "8", expected: "8 is not prime because it is not divisible by 2"},
+		{name: "negative", input: "-1", expected: "Negative numbers are not prime numbers"},
+		{name: "zero", input: "0", expected: "0 is not a prime number"},
+		{name: "one", input: "1", expected: "1 is not a prime number"},
+	}
+	for _, e := range tests {
+
+		input := strings.NewReader(e.input)
+		reader := bufio.NewScanner(input)
+		res, _ := checkNumbers(reader)
+		if !strings.EqualFold(res, e.expected) {
+			t.Errorf("%s: expected %s, but got %s", e.name, e.expected, res)
+		}
+	}
+}
+
+func Test_readUserInput(t *testing.T) {
+	// to test this function we need a channel and an instance of io.reader
+	doneChan := make(chan bool)
+
+	// create a reference to a bytes.buffer
+	var stdin bytes.Buffer
+
+	stdin.Write([]byte("1\nq\n"))
+
+	go readUserInput(&stdin, doneChan)
+	<-doneChan
+	close(doneChan)
 }
